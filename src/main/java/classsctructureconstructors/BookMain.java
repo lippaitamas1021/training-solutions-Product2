4f9e59924762d792248be1a8ee7b128696b39a24
lippaitamas1021 <lippaitamas1021@gmail.com>
2020-11-02 09:01:03 +0100

    book commit

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,13 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("Steve Berry", "A Romanov-jóslat");
+
+        book.register("1234");
+
+        System.out.println("Szerző: " + book.getAuthor());
+        System.out.println("Cím: " + book.getTitle());
+        System.out.println("Regisztrációs szám: " + book.getRegNumber());
+    }
+}
